f8ff510e90ceda4c28fa9b48cd4ebca25525919c
joilson <joilsonmirandasl@hotmail.com>
2020-04-28 14:32:46 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,61 @@
+const res = document.getElementById("res");
+
+const num1 = document.getElementById("btn1").addEventListener("click", adicionar1 => {
+  const n1 = 1;
+  
+  return res.innerHTML += n1;
+});
+
+const num2 = document.getElementById("btn2").addEventListener("click", adicionar2 => {
+  const n2 = 2;
+
+  return res.innerHTML += n2;
+});
+
+const num3 = document.getElementById("btn3").addEventListener("click", adicionar3 => {
+  const n3 = 3;
+
+  return res.innerHTML += n3;
+});
+
+const num4 = document.getElementById("btn4").addEventListener("click", adicionar4 => {
+  const n4 = 4;
+
+  return res.innerHTML += n4;
+});
+
+const num5 = document.getElementById("btn5").addEventListener("click", adicionar5 => {
+  const n5 = 5;
+
+  return res.innerHTML += n5;
+});
+
+const num6 = document.getElementById("btn6").addEventListener("click", adicionar6 => {
+  const n6 = 6;
+
+  return res.innerHTML += n6;
+});
+
+const num7 = document.getElementById("btn7").addEventListener("click", adicionar7 => {
+  const n7 = 7;
+
+  return res.innerHTML += n7;
+});
+
+const num8 = document.getElementById("btn8").addEventListener("click", adicionar8 => {
+  const n8 = 8;
+
+  return res.innerHTML += n8;
+});
+
+const num9 = document.getElementById("btn9").addEventListener("click", adicionar9 => {
+  const n9 = 9;
+
+  return res.innerHTML += n9;
+});
+
+const num0 = document.getElementById("btn0").addEventListener("click", adicionar0 => {
+  const n0 = 0;
+
+  return res.innerHTML += n0;
+});
